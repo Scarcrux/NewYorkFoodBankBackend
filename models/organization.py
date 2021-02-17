2a8c3324e9648f78600dfd00c0a8ae61d487432f
Scarcrux <reg@scarcrux.com>
2021-02-17 14:31:31 -0500

    Add Get Organization Route

--- models/organization.py
+++ models/organization.py
@@ -1,33 +1,27 @@
 from app import db
-
 class Organization(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     organization_name = db.Column(db.String(80))
     contact_name = db.Column(db.String(20))
-    phone = db.Column(db.String(10))
+    phone = db.Column(db.String(14))
     type = db.Column(db.String(20))
     address = db.Column(db.String(80))
-    geocode = db.Column(db.String(80))
-    hours = db.Column(db.String(200))
+    url = db.Column(db.String(40))
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def __init__(self,organization_name, contact_name, phone, type, address, geocode, hours, user_id):
+    def __init__(self, organization_name, type, address, phone, url, user_id):
         self.organization_name = organization_name
-        self.contact_name = contact_name
         self.phone = phone
         self.type = type
         self.address = address
-        self.geocode = geocode
-        self.hours = hours
+        self.url = url
         self.user_id = user_id
 
     def json(self):
         return {
             'organization_name': self.organization_name,
-            'contact_name': self.contact_name,
             'phone': self.phone,
             'type': self.type,
             'address': self.address,
-            'geocode': self.geocode,
-            'hours': self.hours
+            'url': self.url
         }
--- resources/organization.py
+++ resources/organization.py
@@ -1,9 +1,16 @@
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required
 from models.organization import Organization
-from flask import request
-from app import db
+from flask import request, jsonify
+from app import db, ma
 
+
+class OrganizationSchema(ma.Schema):
+    class Meta:
+        fields = ('id','organization_name','type','address','phone','url','user_id')
+
+organization_schema = OrganizationSchema()
+organizations_schema = OrganizationSchema(many=True)
 class Organizations(Resource):
     def get(self,organization_name):
         organization = Organization.query.filter_by(organization_name=organization_name).first()
@@ -22,15 +29,13 @@ class AddOrganization(Resource):
     def post(self):
         args=request.get_json(force=True)
         organization_name = args['organization_name']
-        contact_name = args['contact_name']
         phone = args['phone']
         type = args['type']
         address = args['address']
-        geocode = args['geocode']
-        hours = args['hours']
+        url = args['url']
         user_id = args['user_id']
-        organization = Organization(organization_name = organization_name, contact_name = contact_name, phone = phone, type = type,
-        address = address, geocode = geocode, hours = hours, user_id=user_id)
+        organization = Organization(organization_name = organization_name, phone = phone, type = type,
+        address = address, url = url, user_id = user_id)
         db.session.add(organization)
         db.session.commit()
         return organization.json()
@@ -41,20 +46,16 @@ class EditOrganization(Resource):
         args=request.get_json(force=True)
         organization_id = args['id']
         organization_name = args['organization_name']
-        contact_name = args['contact_name']
         phone = args['phone']
         type = args['type']
         address = args['address']
-        geocode = args['geocode']
-        hours = args['hours']
+        url = args['url']
         organization = Organization.query.filter_by(id=organization_id).first()
         organization.organization_name = organization_name
-        organization.contact_name = contact_name
         organization.phone = phone
         organization.type = type
         organization.address = address
-        organization.geocode = geocode
-        organization.hours = hours
+        organization.url = url
         db.session.commit()
         return organization.json()
 
@@ -64,8 +65,13 @@ class AllOrganization(Resource):
         return [organization.json() for organization in allorganizations ]
 
 class MyOrganization(Resource):
-    def get(self):
-        args=request.get_json(force=True)
-        user_id = args['user_id']
-        allorganizations=Organization.query.filter_by(user_id = user_id)
-        return [organization.json() for organization in allorganizations ]
+    def get(self, user_id):
+        shares = db.session.query(Organization).filter_by(user_id = user_id)
+        result = organizations_schema.dump(shares)
+        return jsonify(result)
+
+class AnOrganization(Resource):
+    def get(self, id):
+        shares = db.session.query(Organization).filter_by(id = id)
+        result = organizations_schema.dump(shares)
+        return jsonify(result)
